ff236bb64ab6c6f5adba95f36e7314597f3af36c
Davindra Sutraban <razrsharp25@gmail.com>
2013-09-18 11:25:28 -0400

    Added BNRMapPoint class

---
+++ Whereami/BNRMapPoint.h
@@ -0,0 +1,13 @@
+//
+//  BNRMapPoint.h
+//  Whereami
+//
+//  Created by Davindra Sutraban on 9/18/13.
+//  Copyright (c) 2013 Davindra Sutraban. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BNRMapPoint : NSObject
+
+@end
---
+++ Whereami/BNRMapPoint.m
@@ -0,0 +1,13 @@
+//
+//  BNRMapPoint.m
+//  Whereami
+//
+//  Created by Davindra Sutraban on 9/18/13.
+//  Copyright (c) 2013 Davindra Sutraban. All rights reserved.
+//
+
+#import "BNRMapPoint.h"
+
+@implementation BNRMapPoint
+
+@end
