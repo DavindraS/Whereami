f1770022dc37ef2dc35b2339acc96fdb8af7d7fc
Davindra Sutraban <razrsharp25@gmail.com>
2013-09-17 21:27:24 -0400

    Added viewDidLoad method and transferred locationManager code to here

--- Whereami/WhereamiViewController.m
+++ Whereami/WhereamiViewController.m
@@ -3,7 +3,6 @@
 //  Whereami
 //
 //  Created by Davindra Sutraban on 9/17/13.
-//  Copyright (c) 2013 Davindra Sutraban. All rights reserved.
 //
 
 #import "WhereamiViewController.h"
@@ -14,29 +13,31 @@
 {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
     
-    if (self) {
-        
-        // Create location manager object
-        locationManager = [[CLLocationManager alloc] init];
-        
-        // Set the desired accuracy to be the most accurate
-        [locationManager setDesiredAccuracy:kCLLocationAccuracyBest];
-        
-        // Tell the manager to start looking for its location immediately
-        [locationManager startUpdatingLocation];
-    }
-    
     return self;
 }
 
-// Since the method in the book has been deprecated. Docs say to use this method
+- (void)viewDidLoad
+{
+    // Create location manager object
+    locationManager = [[CLLocationManager alloc] init];
+    
+    // Set this object to be the delegate for the CLLocationManager
+    [locationManager setDelegate:self];
+    
+    // Set the desired accuracy to be the most accurate
+    [locationManager setDesiredAccuracy:kCLLocationAccuracyBest];
+    
+    // Tell the manager to start looking for its location immediately
+    [locationManager startUpdatingLocation];
+    
+}
+
+// Method locationManager:didUpdateToLocation:fromLocation is deprecated
+// Replaced by locationManager:didUpdateLocations
 - (void)locationManager:(CLLocationManager *)manager
      didUpdateLocations:(NSArray *)locations
 {
-    // The locations array will have CLLocation objects in it
-    // I get the most recent location (at the end of the array)
-    // and get it's description
-    NSLog(@"%@", [[locations objectAtIndex:[locations count]] description]);
+    NSLog(@"%@", [[locations objectAtIndex:([locations count] - 1)] description]);
 }
 
 - (void)locationManager:(CLLocationManager *)manager
